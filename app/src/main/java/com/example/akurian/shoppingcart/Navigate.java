17d931d78469e704c3d8bc2a074990a20c8033f1
anukurian16 <akurian@INakurian.local>
2016-04-02 16:21:36 +0530

    shop matrix added

--- app/src/main/java/com/example/akurian/shoppingcart/Navigate.java
+++ app/src/main/java/com/example/akurian/shoppingcart/Navigate.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -20,7 +19,7 @@ public class Navigate extends AppCompatActivity {
     ArrayList<Integer> listOfNodes;
     ListView mylistview;
     ArrayAdapter<Integer> listAdapter;
-    Node[] routingTable = new Node[20];
+    Node[] routingTable = new Node[30];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,33 +43,92 @@ public class Navigate extends AppCompatActivity {
             String s= list.get(j);
             System.out.println(s);
             switch(s){
-                case "fruits":
+                case "B":
                     System.out.println("case 1");
                     listOfNodes.add(1);
                     break;
-                case "biscuits":
+                case "C":
                     System.out.println("case 2");
                     listOfNodes.add(2);
                     break;
-                case "vegetables":
+                case "D":
                     System.out.println("case 3");
                     listOfNodes.add(3);
                     break;
-                case "beverages":
+                case "E":
                     System.out.println("case 4");
                     listOfNodes.add(4);
                     break;
-                case "bakery":
+                case "F":
                     listOfNodes.add(5);
                     break;
-                case "meat,fish& poultry":
+                case "G":
                     listOfNodes.add(6);
                     break;
-                case "household":
+                case "H":
                     listOfNodes.add(7);
                     break;
-                case "cereals":
+                case "I":
                     listOfNodes.add(8);
+                    break;
+                case "J":
+                    System.out.println("case 1");
+                    listOfNodes.add(9);
+                    break;
+                case "K":
+                    System.out.println("case 2");
+                    listOfNodes.add(10);
+                    break;
+                case "L":
+                    System.out.println("case 3");
+                    listOfNodes.add(11);
+                    break;
+                case "M":
+                    System.out.println("case 4");
+                    listOfNodes.add(12);
+                    break;
+                case "N":
+                    listOfNodes.add(13);
+                    break;
+                case "O":
+                    listOfNodes.add(14);
+                    break;
+                case "P":
+                    listOfNodes.add(15);
+                    break;
+                case "Q":
+                    listOfNodes.add(16);
+                    break;
+                case "R":
+                    System.out.println("case 1");
+                    listOfNodes.add(17);
+                    break;
+                case "S":
+                    System.out.println("case 2");
+                    listOfNodes.add(18);
+                    break;
+                case "T":
+                    System.out.println("case 3");
+                    listOfNodes.add(19);
+                    break;
+                case "U":
+                    System.out.println("case 4");
+                    listOfNodes.add(20);
+                    break;
+                case "V":
+                    listOfNodes.add(21);
+                    break;
+                case "W":
+                    listOfNodes.add(22);
+                    break;
+                case "X":
+                    listOfNodes.add(23);
+                    break;
+                case "Y":
+                    listOfNodes.add(24);
+                    break;
+                case "Z":
+                    listOfNodes.add(25);
                     break;
             }
 
@@ -106,14 +164,12 @@ public class Navigate extends AppCompatActivity {
 
     public ArrayList<Integer> bellman(ArrayList<Integer> listOfNodes)throws Exception{
 
-        int adjacencyMatrix[][] = new int[20][20];
+        int adjacencyMatrix[][] = new int[30][30];
         int min, pos, cur=0, next, prev, x;
         ArrayList<Integer> tempList = new ArrayList<Integer>();
         ArrayList<Integer> path = new ArrayList<Integer>();
         Iterator iterator = path.iterator();
-        //InputStream inputStream=openFileInput("data.txt");
-        //InputStream input = getAssets().open("numbers.txt");
-        Scanner scanner = new Scanner(getAssets().open(String.format("data.txt")));
+        Scanner scanner = new Scanner(getAssets().open(String.format("data2.txt")));
         //reader = new Scanner(new File(getAssets().open(String.format("myFile.txt"))));
 
         Log.e("in func","bellman");
